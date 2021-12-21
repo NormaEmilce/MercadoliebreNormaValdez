c047f3e3406b396ef90cf0b01c116a32378a64ca
NormaEmilce <norma-v@hotmail.com>
2021-12-21 18:47:19 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+app.use( express.static(publicPath) );
+
+app.listen(  process.env.PORT ||   3000,()=> {
+    console.log("el servidor esta corriendo en el puerto 3000")
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+})
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'./views/register.html'));
+})
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'./views/login.html'));  
+})
