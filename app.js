8c873ba04c390247705a4052b87cee4c809917ac
NormaEmilce <norma-v@hotmail.com>
2021-12-28 18:48:08 -0300

    correciones en app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 const publicPath = path.resolve(__dirname, './public');
-app.use( express.static(publicPath) );
+app.use(express.static(publicPath) );
 
 app.listen(process.env.PORT || 3000,()=> {
     console.log("el servidor esta corriendo en el puerto 3000")
