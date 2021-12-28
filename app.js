bbb9e98e0237e2c265caef5d8c7a8ce92a157eb7
NormaEmilce <norma-v@hotmail.com>
2021-12-28 18:47:15 -0300

    correcciones en app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.listen(process.env.PORT || 3000,()=> {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'));
+    res.sendFile(path.resolve(__dirname,'./views/home.html'));
 })
 app.get('/register', (req, res) => {
     res.sendFile(path.resolve(__dirname,'./views/register.html'));
